1f14f32987c4a5c3dfb73939a639400f771de30f
Rustam Karimov <rkarimov.r@gmail.com>
2019-03-02 23:22:46 +0400

    Add proxy pattern example

---
+++ structural/proxy/src/main/java/proxy/example/Demo.java
@@ -0,0 +1,34 @@
+package proxy.example;
+
+import proxy.example.downloader.YoutubeDownloader;
+import proxy.example.library.ThirdPartyYoutubeLibImpl;
+import proxy.example.proxy.YoutubeCacheProxy;
+
+public class Demo {
+    public static void main(String[] args) {
+        YoutubeDownloader naiveDownloader = new YoutubeDownloader(new ThirdPartyYoutubeLibImpl());
+        YoutubeDownloader smartDownloader = new YoutubeDownloader(new YoutubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+
+    private static long test(YoutubeDownloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
